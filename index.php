b8f53a7124de134a299abcdfb08f22709932b038
Luca Somaschini <somas99@gmail.com>
2022-09-22 15:39:32 +0200

    First movie: done

--- index.php
+++ index.php
@@ -2,4 +2,25 @@
 
 require_once __DIR__ . '/movie_class.php';
 
-echo $prova;
+$movie1 = new Movie('Memento', 2000, 'Christopher Nolan', 'Thriller');
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP OOP: Class.</title>
+</head>
+<body>
+    <h1>First movie:</h1>
+    <ul>
+        <li>Title: <?php echo $movie1->getTitle() ?></li>
+        <li>Year: <?php echo $movie1->getYear() ?></li>
+        <li>Director: <?php echo $movie1->getDirector() ?></li>
+        <li>Genre: <?php echo $movie1->getGenre() ?></li>
+        <li>Original Language: <?php echo $movie1->getOriginalLanguage() ?></li>
+    </ul>
+</body>
+</html>
--- movie_class.php
+++ movie_class.php
@@ -1,3 +1,42 @@
 <?php
 
-$prova = 'ciao';
+class Movie {
+    public $title;
+    public $year;
+    public $director;
+    public $genre;
+    protected $original_language;
+
+    public function __construct($titleP, $yearP, $directorP, $genreP = null)
+    {
+        $this->title = $titleP;
+        $this->year = $yearP;
+        $this->director = $directorP;
+        $this->genre = $genreP;
+        $this->setOriginalLanguage();
+    }
+
+    public function getTitle() {
+        return $this->title;
+    }
+    
+    public function getYear() {
+        return $this->year;
+    }
+    
+    public function getDirector() {
+        return $this->director;
+    }
+    
+    public function getGenre() {
+        return $this->genre;
+    }
+
+    protected function setOriginalLanguage() {
+        $this->original_language = 'en';
+    }
+
+    public function getOriginalLanguage() {
+        return $this->original_language;
+    }
+}
